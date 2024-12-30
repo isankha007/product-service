8728f48655de164b06fb03adc6b69abfbf7d6773
Sankhadeep Chatterjee <sankhadeepchatterjee@Sankhadeeps-MacBook-Air.local>
2024-12-30 09:12:51 +0530

    change service url for docker

--- src/main/java/com/sankha/productService/config/AppConstants.java
+++ src/main/java/com/sankha/productService/config/AppConstants.java
@@ -4,7 +4,7 @@ public class AppConstants {
     public static final String CART_ADD_ITEM_EVENT = "cart_event";
     public static final String DEDUCT_QUANTITY = "deduct-quantity";
     public static final String INCREASE_QUANTITY = "increase-quantity";
-    public static final String USERS_VERIFY =  "http://localhost:8082/users/verify"; //"http://user-service:8082/users/verify";
+    public static final String USERS_VERIFY =  "http://user-service:8082/users/verify";
     public static final String SUCCESS = "Success";
     public static final String FAILED = "Failed";
 }
